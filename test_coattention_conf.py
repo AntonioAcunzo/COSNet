866559e3550e1f7c0674149dbe0b70eda8d73b4b
AntonioAcunzo <acu8397@gmail.com>
2021-07-12 23:46:47 +0200

    step 2

--- test_coattention_conf.py
+++ test_coattention_conf.py
@@ -715,7 +715,7 @@ def main():
                 f_results.write("\n ACC SUMMARY FINAL\n")
                 f_results.write(str(strsummary))
 
-                M = strsummary.as_matrix()
+                M = summary.as_matrix()
                 for i in M:
                     print(i)
 
